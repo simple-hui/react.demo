c5562bf9f792bf776bfb459cfe54e844b01ab6ef
simple-hui <276557421@qq.com>
2018-04-09 14:24:49 +0800

    初始化工程

---
+++ demo/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        demo
+      </div>
+    );
+  }
+}
+
+export default App;

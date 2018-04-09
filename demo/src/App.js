8ff93494a3257df2bf27d42084a8592906de40a1
simple-hui <276557421@qq.com>
2018-04-09 15:41:05 +0800

    尝试组件嵌套

--- demo/src/App.js
+++ demo/src/App.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
+// import { Router, Rroute, Switch } from 'react-router';
+
+//增加路由页面
+import Home from './components/Home';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        demo
+      <div>
+        <Home />
       </div>
     );
   }
 }
+
+// React.render({
+// 	<Router>
+// 		<Route path="/" component={App}>
+// 			<Route path="/home" component={home}></Route>
+// 		</Route>
+// 	</Router>
+// })
 
 export default App;

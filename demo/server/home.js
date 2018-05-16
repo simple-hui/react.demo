4af99dae1927d2653d9cc8a5155a67769bbf7c6b
simple-hui <276557421@qq.com>
2018-05-16 10:27:12 +0800

    node服务增加热更新

--- demo/server/home.js
+++ demo/server/home.js
@@ -10,14 +10,12 @@ router.post('/api/home.do',function (req, res,next) {
 	console.log(req.body);
 	let id = req.body.id;
 	console.log(id);
-	connection.query(`SELECT homecol FROM home where id = ${id}`,function(error,rows) {
+	connection.query(`SELECT * FROM home where id = ${id}`,function(error,rows) {
 		if (error){
-			console.log("执行错误");
-			console.log(error);
+			console.log("执行错误,获取数据列表异常");
 			next();
 		} else {
-			console.log(rows);
-			return res.status(200).json({msg: rows,success: true,});
+			return res.status(200).json({data: rows,success: true,});
 		}
 	})
 	
--- demo/src/components/Home.js
+++ demo/src/components/Home.js
@@ -3,13 +3,24 @@ import axios from 'axios';
 import './home.css';
 
 class Home extends Component {
-	//初始化钩子函数
+	// 控制状态
+	constructor(props) {
+		super(props);
+		this.state = {
+			data: []
+		};
+	}
+	// 初始化钩子函数
 	componentDidMount() {
 		// alert();
 		axios.post('/api/home.do',{
 			id: '111'
 		}).then(resp=>{
-			console.log(resp);
+			// react中赋值使用的setState
+			this.setState({
+				data: resp.data.data
+			})
+			console.log(this.state.data);
 		})
 	}
 
@@ -20,6 +31,16 @@ class Home extends Component {
     return (
       <div className="home">
         <img src={require("../assets/images/home.jpeg")} alt="home" className="homeImg" />
+        <div className="homeDetail">
+					<ul>
+						for ( let i in data ) {
+							<li>
+							
+							</li>
+						}
+						
+					</ul>
+        </div>
       </div>
     );
   }
